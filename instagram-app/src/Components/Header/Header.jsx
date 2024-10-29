81aae4488998243f9145f4266c9e96f1c3899b87
Ahmed <amartazanov892@gmail.com>
2024-10-29 15:56:31 +0300

    main commit 7

--- instagram-app/src/Components/Header/Header.jsx
+++ instagram-app/src/Components/Header/Header.jsx
@@ -1,72 +1,82 @@
-import React from "react";
+import React, {useState} from "react";
 import "./header.css";
+import Stories from "./Stories";
 
 
 function Header() {
     const stories = [
         {
             userName: 'Muhammed',
-            img: 'img/amg.jpg'
+            avatar: 'img/amg.jpg',
+            imgstories: ['img/granta.jpg']
         },
         {
             userName: 'Arsamak',
-            img: 'img/audi.jpg'
+            avatar: 'img/audi.jpg',
+            imgstories: ['img/granta.jpg']
         },
         {
             userName: 'Adam',
-            img: 'img/granta.jpg'
+            avatar: 'img/granta.jpg',
+            imgstories: ['img/granta.jpg']
         },
         {
             userName: 'Musa',
-            img: 'img/bugatti.jpg'
+            avatar: 'img/bugatti.jpg',
+            imgstories: ['img/granta.jpg']
         },
         {
             userName: 'Muhammed',
-            img: 'img/amg.jpg'
+            avatar: 'img/amg.jpg',
+            imgstories: ['img/granta.jpg']
         },
         {
             userName: 'Arsamak',
-            img: 'img/audi.jpg'
+            avatar: 'img/audi.jpg',
+            imgstories: ['img/granta.jpg']
         },
         {
             userName: 'Adam',
-            img: 'img/granta.jpg'
+            avatar: 'img/granta.jpg',
+            imgstories: ['img/granta.jpg']
         },
         {
             userName: 'Musa',
-            img: 'img/bugatti.jpg'
+            avatar: 'img/bugatti.jpg',
+            imgstories: ['img/granta.jpg']
         }
     ]
 
-    const styles = {
-        cursor: 'pointer'
-    }
+    
+
+    const [openStories, setOpenStories] = useState(false);
     return (
         <header>
+            {openStories && <div className="stories-block"><img src="img/granta.jpg" alt="" /></div>}
             <div class="head-block">
                 <img class="instaLogo" src='img/Instagram_logo.svg' alt="" />
                 <div class="icons">
-                <img src='img/heartIcon.svg' alt="" style={styles}/>
-                <img src='img/shareIcon.svg' alt="" style={styles}/>
+                <img src='img/heartIcon.svg' alt="" style={{cursor: 'pointer'}}/>
+                <img src='img/shareIcon.svg' alt="" style={{cursor: 'pointer'}}/>
                 </div>
             </div>
 
-            <div class="stories">
-                <div class="storis">
-                    <div class="stiorisImgBl">
-                        <img class="storisImg" src='img/bmw.jpg' alt="" />
+            <div class="stories-contein">
+                <div class="storiesBtn">
+                    <div class="storiesImg-block">
+                        <img class="storiesImg" src='img/bmw.jpg' alt="" />
                     </div>
                     <img class="addStory" src='img/addStory.svg' alt="" />
-                    <span class="storisName1">Your story</span>
+                    <span class="storiesName">Your story</span>
                 </div>
 
                 {
                     stories.map(str => (
-                        <div class="storis">
-                            <div class="stiorisImgBl">
-                                <img class="storisImg" src={str.img} alt="" />
+                        <div class="storiesBtn" onClick={() => setOpenStories(true)}>
+                            <div class="storiesImg-block">
+                                <img class="storiesImg" src={str.avatar} alt="" />
                             </div>
-                            <span class="storisName2">{str.userName}</span>
+                            <span class="storiesName">{str.userName}</span>
                         </div>
                     ))
                 }
---
+++ instagram-app/src/Components/Header/Stories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Stories(params){
+
+    return(
+        <div className="screen-stories">
+
+        </div>
+    )
+}
+
+export default Stories;
